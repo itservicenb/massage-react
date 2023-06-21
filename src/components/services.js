2dc64f6d1d13d9521dc63e5e7e1a1d2cb0fe68eb
ĐINH VĂN CƯƠNG <cuongdv@devcamp.edu.vn>
2023-06-21 11:29:43 +0700

    update service mobile

--- src/components/services.js
+++ src/components/services.js
@@ -96,13 +96,12 @@ class Services extends Component {
                                             'url(https://github.com/jdleo/Lauren-Massage-Therapy/blob/master/src/images/deeptissue.jpg?raw=true) center / cover',
                                     }}
                                 >
-                                    Deep Tissue Massage
+                                    Massage Đầu, vai gáy, tay chân
                                 </CardTitle>
                                 <CardText>
-                                    Deep tissue massage therapy is similar to Swedish massage, but the deeper pressure
-                                    is beneficial in releasing chronic muscle tension. The focus is on the deepest
-                                    layers of muscle tissue, tendons and fascia (the protective layer surrounding
-                                    muscles, bones and joints).
+                                    Quý khách sẽ được trải nghiệm và sử dụng dịch vụ trong vòng 60 phút, với kỹ thuật
+                                    massage chuyên nghiệp từ những nhân viên được đào tạo bài bản. Giúp quý khách có
+                                    những phút giây thư giãn thật thăng hoa và thoải mái.
                                 </CardText>
                             </Card>
                         </Cell>
@@ -116,17 +115,10 @@ class Services extends Component {
                                             'url(https://github.com/jdleo/Lauren-Massage-Therapy/blob/master/src/images/swedish.jpeg?raw=true) center / cover',
                                     }}
                                 >
-                                    Swedish Massage
+                                    Massage Đầu, vai gáy, tay, chân, Lưng đá nóng.
                                 </CardTitle>
                                 <CardText>
-                                    Swedish massage therapy is the modality that comes to mind when most people think
-                                    about massage. As the best-known type of bodywork performed today, one of the
-                                    primary goals of the Swedish massage technique is to relax the entire body. This is
-                                    accomplished by rubbing the muscles with long gliding strokes in the direction of
-                                    blood returning to the heart. But Swedish massage therapy goes beyond relaxation.
-                                    Swedish massage is exceptionally beneficial for increasing the level of oxygen in
-                                    the blood, decreasing muscle toxins, improving circulation and flexibility while
-                                    easing tension.
+                                    Với 90 phút trải nghiệm dịch vụ sẽ khiến quý khách thật thư thái và hài lòng.
                                 </CardText>
                             </Card>
                         </Cell>
@@ -140,13 +132,12 @@ class Services extends Component {
                                             'url(https://github.com/jdleo/Lauren-Massage-Therapy/blob/master/src/images/sports.jpg?raw=true) center / cover',
                                     }}
                                 >
-                                    Sports Massage
+                                    Massage body toàn thân.
                                 </CardTitle>
                                 <CardText>
-                                    Sports massage therapy is geared toward athletes of every kind, from world-class
-                                    professionals to weekend joggers. The particulars of the sports massage technique
-                                    are specific to the athlete's sport of choice. Focusing on areas of the body that
-                                    are overused and stressed from repetitive and often aggressive movements.
+                                    Với 120 phút trải nghiệm toàn bộ dịch vụ từ A đến Z quý khách sẽ được thả lỏng thư
+                                    giãn toàn bộ cơ thể. Những cơn mệt mỏi cơ bắp, vai gáy, toàn thân sẽ tan biến một
+                                    cách kì diệu sau khi được các chuyên viên của Hoài Phương massage chăm sóc.
                                 </CardText>
                             </Card>
                         </Cell>
@@ -160,11 +151,10 @@ class Services extends Component {
                                             'url(https://github.com/jdleo/Lauren-Massage-Therapy/blob/master/src/images/reflexology.jpeg?raw=true) center / cover',
                                     }}
                                 >
-                                    Reflexology
+                                    Dịch vụ khác.
                                 </CardTitle>
                                 <CardText>
-                                    Reflexology massage can be a deeply relaxing and therapeutic modality for those
-                                    suffering from plantar fasciitis, ankle injuries or even everyday work and play.
+                                    Đến với HOÀI PHƯƠNG quý khách có thể sử dụng dịch vụ theo nhu cầu cá nhân riêng.
                                 </CardText>
                             </Card>
                         </Cell>
